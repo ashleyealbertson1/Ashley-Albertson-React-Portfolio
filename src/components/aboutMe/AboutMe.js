cdb544d12695b87ef06be13dfd18618276ee4d13
Ashley Albertson <99622525+ashleyealbertson1@users.noreply.github.com>
2022-12-13 15:53:45 -0600

    Update AboutMe.js

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -49,11 +49,8 @@ const AboutMe = () => {
                                                             <div className="about-me">
                                                                 <div className="about-me-desc">Full Stack Software Developer with a passion for coding. Dedicated to continuous
 //                                         learning and excited by challenges that increase programming skills and knowledge. Strive to continually
-//                                         evolve as a developer to become an invaluable asset to the company that brings me on board.
-
-//                                         Qualified for both
-//                                         front-end and back-end Junior-to-Mid Level Software Engineer roles. Knowledgeable in JavaScript,
-//                                         Node.js, CSS, React.js, MySQL, MongoDB as well as a few other in-demand applications.
+//                                         evolve as a developer. 
+//                                         Knowledgeable in JavaScript, Python, SQL, Object Relational Mapping, Django, Flask, Node.js, CSS, React.js, MySQL, MongoDB as well as a few other in-demand applications.
 
 //                                         Graduated from SMU Full Stack Software Engineering Program in August 2022. Previous professional career
 //                                         of nearly a decade in results-driven Sales and Marketing leadership roles. 2 years traditional sales. 5 years
@@ -61,9 +58,7 @@ const AboutMe = () => {
 //                                         campaigns and heading up Online Sales efforts in small multi-million-dollar companies and large global
 //                                         corporations for both business-to-customer and business-to-business channels.
 
-//                                         Celebrated numerous professional and personal achievements in life but greatest achievement and greatest
-//                                         joy is being a mom. Top priority in life is to raise a wonderful young man who will bring more kindness
-//                                         and compassion into the world.</div>
+//                                        </div>
 
 
 
